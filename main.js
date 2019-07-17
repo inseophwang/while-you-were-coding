5a396ea8f480b1d1512958ed54b65927da712fb2
In Seop Hwang <inseop@ins-macbook-pro.lan>
2019-07-17 14:53:32 -0400

    completed challenge #1

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ console.log('Challenge #1');
 
 // Use `if` and a modulus operation.
 
+let i = 1;
+
+while(i <= 20){
+    if(i % 3 === 0){
+        console.log(i);
+    }
+
+    i = i + 1;
+}
+
 
 console.log('Challenge #2');
 
